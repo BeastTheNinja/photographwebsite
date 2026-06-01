3908649850421fb6f27da83ba621f2f828e2c797
BeastTheNinja <BeastTheNinja21@outlook.com>
2026-06-01 17:13:28 +0200

    missing Analytics

--- src/components/layout/performance-insights.tsx
+++ src/components/layout/performance-insights.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function PerformanceInsights() {
-    return <SpeedInsights />;
+    return (
+        <>
+            <SpeedInsights />
+            <Analytics />
+        </>
+    )
 }
